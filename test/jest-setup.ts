fbc981adc8bd5cbc46e8f7a83d8fe486665f6930
J3eff <jeffersonvideo125@gmail.com>
2023-06-18 14:06:20 -0300

    Node.js API [C04P01] - Configurando MongoDB com Node js e Typescript

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,9 +1,12 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
+let server: SetupServer;
 // Vai ser executado antes de todos os testes, para realizar o setup do server
-beforeAll(() => {
-  const server = new SetupServer();
-  server.init();
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
   global.testRequest = supertest(server.getApp());
 });
+
+afterAll(async() => await server.close());
